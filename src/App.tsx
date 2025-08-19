1254c583933e5178836cd15e1bdc0172d1f1c339
David <davidhurmanchuk@gmail.com>
2025-08-19 23:24:58 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRed } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [visibleGoods, setVisibleGoods] = useState<Good[]>([]);
+  const [error, setError] = useState<string | null>(null)
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleSuccess = (goods: Good[]) => {
+    setVisibleGoods(goods);
+    setError(null);
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleError = () => {
+    setError('Failed to load goods');
+    setVisibleGoods([]);
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleGetAll = () => {
+    getAll()
+      .then(handleSuccess)
+      .catch(handleError);
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  const handleGet5First = () => {
+    get5First()
+      .then(handleSuccess)
+      .catch(handleError);
+  };
+
+  const handleGetRed = () => {
+    getRed()
+      .then(handleSuccess)
+      .catch(handleError);
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" data-cy="all-button" onClick={handleGetAll}>
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleGet5First}
+      >
+        Load 5 first goods
+      </button>
+
+      <button type="button" data-cy="red-button" onClick={handleGetRed}>
+        Load red goods
+      </button>
+      {error && <p className="error">{error}</p>}
+      <GoodsList goods={visibleGoods} />
+    </div>
+  );
+};
